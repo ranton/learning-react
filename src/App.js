335711fe8e52445b3d0e729f979d5884070bd482
ranton <fdtacata@gmail.com>
2022-04-09 17:56:53 +0800

    Added React props

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
 import * as React from 'react';
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 }
 
-const List = () => {
+const List = (props) => {
   return (
     <ul>
-      {list.map((item) => {
+      {props.list.map((item) => {
         return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+          <Item key={item.objectID} item={item} />
         );
       })}
     </ul>
+  );
+}
+
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
   );
 }
 
@@ -55,7 +62,7 @@ const Search = () => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 }
