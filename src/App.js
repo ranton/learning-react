cbe6298a04a1c16ed19c93d97ca7073895652754
ranton <fdtacata@gmail.com>
2022-04-09 17:24:27 +0800

    Added Meet another react component

--- src/App.js
+++ src/App.js
@@ -21,12 +21,19 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (<li key={item.objectID}>
+      <List />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
@@ -34,9 +41,17 @@ function App() {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
           </li>
-          );
-        })}
-      </ul>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
