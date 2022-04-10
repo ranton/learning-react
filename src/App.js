3a2c37bcb521d809b2bbed58a23f22f2efba06b1
ranton <fdtacata@gmail.com>
2022-04-10 21:46:59 +0800

    Added Data fetching with React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 const useSemiPersistentState = (key, intialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || intialState
@@ -44,40 +46,7 @@ const storiesReducer = (state, action) => {
   }
 }
 
-const App = () => {
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  // const getAsyncStories = () =>
-  //   new Promise((resolve) =>
-  //     setTimeout(
-  //       () => resolve({ data: { stories: initialStories } }),
-  //       2000
-  //     )
-
-  //   );
-
-  const getAsyncStories = () =>
-    new Promise((resolve, reject) => setTimeout(reject, 2000));
-
-
-
+const App = () => {    
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const [stories, dispatchStories] = React.useReducer(
@@ -86,19 +55,20 @@ const App = () => {
   );
 
   React.useEffect(() => {
-    dispatchStories({ type: 'STORIES_FETCH_INIT'})
+    dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
-          });        
+          payload: result.hits,
+        });
       })
       .catch(
-        () => dispatchStories({ type: 'STORIES_FETCH_FAILURE'})
-      )
-  }, []);
+        () => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      );
+  }, []);  
 
   const handleRemoveStory = (item) => {
     dispatchStories({
