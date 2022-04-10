68d05cc40ce5a68eb04f858d45a9338543024f4e
ranton <fdtacata@gmail.com>
2022-04-10 16:39:42 +0800

    Added Inline handler in JSX

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const useSemiPersistentState = (key, intialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -32,6 +32,15 @@ const App = () => {
   ];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -53,36 +62,41 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 }
 
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
 
   console.log('List renders');
 
   return (
     <ul>
-      {list.map(({ objectID, ...item }) => (
-        <Item key={objectID} {...item} />
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
       ))}
     </ul>
   );
 }
 
-const Item = ({ title, url, author, num_comments, points }) => {
+const Item = ({ item, onRemoveItem }) => {
 
   console.log('Item renders');
 
   return (
     <li>
       <span>
-        <a href={url}>{title}</a>
+        <a href={item.url}>{item.title}</a>
       </span>
-      <span>{num_comments}</span>
-      <span>{author}</span>
-      <span>{points}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.author}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
     </li>
   );
 }
