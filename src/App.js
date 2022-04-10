fe137bbe5da91c9f30c99766fd221dbf3569d9c0
ranton <fdtacata@gmail.com>
2022-04-10 22:31:05 +0800

    Added Async/Await in React

--- src/App.js
+++ src/App.js
@@ -59,22 +59,20 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     if (!searchTerm) return;
 
-    dispatchStories({ type: 'STORIES_FETCH_INIT' })
-
-    axios
-      .get(url)      
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(
-        () => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    
+    try{
+      const result = await axios.get(url);
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    }
   }, [url]);
 
   React.useEffect(() => {
