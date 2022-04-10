e8647ea447ef37a6aafdae60c52c3fc219801104
ranton <fdtacata@gmail.com>
2022-04-10 22:25:59 +0800

    Added Third-party libraries in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -63,12 +64,12 @@ const App = () => {
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)      
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(
